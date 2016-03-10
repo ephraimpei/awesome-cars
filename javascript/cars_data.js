8673fa26795a639f4bdafa61adc75f17a3a33f5e
Ephraim Pei <ephraimpei@gmail.com>
2016-03-10 17:00:34 -0500

    initial commit. folder structure set up. styling: navbar done, cars-options sidebar in progress

---
+++ javascript/cars_data.js
@@ -0,0 +1,25 @@
+const dataSetSize = 100, maxPrice = 100000, minPrice = 10000;
+
+// create an array from 0 to dataSetSize
+const dataSetArray = Array.apply(0, Array(dataSetSize)).map( (undefined, idx) => idx );
+const carColors = ["black", "blue", "green", "red", "silver", "yellow"];
+
+// create the cars data set w/ functional programming techniques :)
+const carsDataSet = dataSetArray.map( (n) => {
+  const color = carColors[(n % carColors.length)];
+  const name = `${ color } car`;
+  const imageURL = `image-car-${ color }-2x.png`;
+  const price = Math.floor(Math.random() * (maxPrice - minPrice + 1)) + minPrice;
+  const priceUSDFormat = "$" + price.toLocaleString();
+
+  return {
+    id: n,
+    color: color,
+    name: name,
+    imageURL: imageURL,
+    price: price,
+    priceUSDFormat: priceUSDFormat
+  };
+});
+
+module.exports = carsDataSet;
