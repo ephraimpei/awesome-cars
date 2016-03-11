c49186c553e1200dd3abb93b0b2ac865053dc2cd
Ephraim Pei <ephraimpei@gmail.com>
2016-03-10 23:06:51 -0500

    HTML / CSS for cars index done.  working on Javascript for loading initial dataset... createDataSet function working correctly

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let carsIndex = $(".cars-index");
+const initDataSetSize = 20;
+const initDataSet = createDataSet(initDataSetSize);
+
+// console.log(dataSet);
+
+const carsIndexItems = initDataSet.map( (car) => {
+
+});
